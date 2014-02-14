dddd75688600a38f9b393ff1e9fd14e87d1f0e56
Feth AREZKI <feth@tuttu.info>
2014-02-14 22:17:19 +0100

    Salt module to build my blog with pelican

---
+++ pelican.py
@@ -0,0 +1,127 @@
+# coding: utf-8
+"""
+    Provide saltstack building for pelican
+
+    http://docs.saltstack.com/ref/modules/index.html#modules-are-easy-to-write
+
+    This file:
+    Copyright: 2014 Majerti
+    License: Apache-2.0 http://www.apache.org/licenses/LICENSE-2.0
+"""
+
+import logging
+import os
+import shlex
+import subprocess
+
+
+from salt import exceptions
+
+
+_has_git = True
+try:
+    from salt.modules import git
+except ImportError:
+    _has_git = False
+
+PELICAN_VENV = '/var/cache/pelican_venv'
+PELICAN_DATA_DIR = "/var/cache/blog_source"
+
+
+_logger = logging.getLogger('pelican_module')
+
+
+def _conditions():
+    """Internal generator: enumerate booleans for requisites.
+
+    Logs a message about what is going to be checked"""
+    logging.debug("Checking for git")
+    yield _has_git
+    logging.debug("Checking for pelican venv installed: %s", PELICAN_VENV)
+    yield os.path.isdir(PELICAN_VENV)
+    logging.debug("Checking for pelican data dir: %s ", PELICAN_DATA_DIR)
+    yield os.path.isdir(PELICAN_DATA_DIR)
+
+
+def __virtual__():
+    """Should this module be loaded?"""
+    if not all(_conditions()):
+        logging.debug("Condition failed")
+        return False
+    return """pelican"""
+
+
+def build():
+    """
+    Build the pelican blog, as soon as I get this module to load.
+
+    Calls update() before.
+    """
+    pelican_update()
+
+    # PWD variable is used by the Makefile
+    env = {
+        'PATH': ':'.join((
+            '%s/bin/' % PELICAN_VENV,
+            os.environ['PATH']
+            )),
+        'PWD': PELICAN_DATA_DIR,
+        }
+    command = 'make html'
+    process = subprocess.Popen(
+        shlex.split(command),
+        env=env,
+        cwd=PELICAN_DATA_DIR,
+        stdout=subprocess.PIPE,
+        stderr=subprocess.PIPE
+        )
+    process.wait() # risk of overfilling PIPEs?
+    stdout_data, stderr_data = process.communicate()
+    returncode = process.returncode
+
+    if returncode == 0:
+        return "pelican salt module, building %s\n%s" % (
+            PELICAN_DATA_DIR,
+            stdout_data
+            )
+    raise exceptions.CommandExecutionError('\n'.join((
+        '"make html" -> ...',
+        'stdout:',
+        stdout_data,
+        'stderr:',
+        stderr_data,
+        'env:',
+        str(env),
+        )))
+
+
+def update():
+    """
+    Updates the git checkout of the blog
+    """
+    return __salt__['git.pull'](PELICAN_DATA_DIR, opts='origin master')
+
+def upgrade():
+    command = "%s/bin/pip install --upgrade pelican" % PELICAN_VENV
+    process = subprocess.Popen(
+        shlex.split(command),
+        cwd=PELICAN_DATA_DIR,
+        stdout=subprocess.PIPE,
+        stderr=subprocess.PIPE
+        )
+    process.wait() # risk of overfilling PIPEs?
+    stdout_data, stderr_data = process.communicate()
+    returncode = process.returncode
+
+    if returncode == 0:
+        return "pelican update:\n%s" % (
+            stdout_data
+            )
+    raise exceptions.CommandExecutionError('\n'.join((
+        '"make html" -> ...',
+        'stdout:',
+        stdout_data,
+        'stderr:',
+        stderr_data,
+        'env:',
+        )))
